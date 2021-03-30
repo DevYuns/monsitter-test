04139be89ec531fbcf8276a14f00502fab0c0e45
DevYuns <ydhun93@gmail.com>
2021-03-30 18:27:05 +0900

    Jwt 모듈 구현/ 로그인 뮤테이션 작성

---
+++ src/jwt/jwt.module.ts
@@ -0,0 +1,22 @@
+import { CONFIG_OPTIONS } from './../common/common.constants';
+import { JwtModuleOptions } from './jwt.interfaces';
+import { JwtService } from './jwt.service';
+import { Module, DynamicModule, Global } from '@nestjs/common';
+
+@Module({})
+@Global()
+export class JwtModule {
+  static forRoot(options: JwtModuleOptions): DynamicModule {
+    return {
+      module: JwtModule,
+      exports: [JwtService],
+      providers: [
+        {
+          provide: CONFIG_OPTIONS,
+          useValue: options,
+        },
+        JwtService,
+      ],
+    };
+  }
+}
