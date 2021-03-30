64a7a86ed0361e6c89e57d8e9885d419b6b60fce
DevYuns <ydhun93@gmail.com>
2021-03-30 14:28:11 +0900

    엔티티 필드 타입 유효성 검사 추가

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -7,6 +7,7 @@ import {
 import { CoreEntity } from './../../common/entities/core.entity';
 import { InternalServerErrorException } from '@nestjs/common';
 import { Entity, Column, BeforeInsert } from 'typeorm';
+import { IsEmail, IsString, IsEnum, IsNumber, IsDate } from 'class-validator';
 import * as bcrypt from 'bcrypt';
 
 enum SexType {
@@ -28,34 +29,42 @@ export class User extends CoreEntity {
   //TODO: 유니크 키로 자동 생성되로록 바꾸기
   @Column({ default: 12345 })
   @Field(() => Number)
+  @IsNumber()
   MemberNumber: number;
 
   @Column()
   @Field(() => String)
+  @IsString()
   name: string;
 
   @Column()
   @Field(() => Date)
+  @IsDate()
   birthday: Date;
 
   @Column({ type: 'enum', enum: SexType })
   @Field(() => SexType)
+  @IsEnum(SexType)
   sex: SexType;
 
   @Column({ unique: true })
   @Field(() => String)
+  @IsString()
   accountId: string;
 
   @Column()
   @Field(() => String)
+  @IsString()
   password: string;
 
   @Column()
   @Field(() => String)
+  @IsEmail()
   email: string;
 
   @Column({ type: 'enum', enum: UserRole, array: true })
   @Field(() => [UserRole])
+  @IsEnum(UserRole)
   role: UserRole[];
 
   @BeforeInsert()
